c0c042a44a56642120749d5324851c03d30f05df
Karthik Vegesna <krvegesna@gmail.com>
2019-10-14 18:09:22 -0700

    dddd enter the commit message for your changes. Lines starting

--- argmax.s
+++ argmax.s
@@ -6,7 +6,7 @@
 #	element. If there are multiple, return the one
 #	with the smallest index.
 # Arguments:
-# 	a0 is the pointer to the start of the vector
+# a0 is the pointer to the start of the vector
 #	a1 is the # of elements in the vector
 # Returns:
 #	a0 is the first index of the largest element
@@ -14,25 +14,42 @@
 argmax:
 
     # Prologue
+    addi sp, sp, -12
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    add s0, x0, a0 #address of pointer
+    lw s1, 0(a1) #size of pointer
+    add s2, x0, x0 #counter of pointer
 
+    addi t0, x0, x0 #biggest
+    addi t1, x0, x0 #smallest
+    addi t2, x0, x0
 
 loop_start:
-
-
-
-
-
-
-
+    addi s2, s2, 1
+    bge s2, s1, loop_end
+    j loop_continue
 
 loop_continue:
-
-
+    lw t0, 0(s0)
+    bgt 0(s0), t0, bigger
+    blt 0(s0), t0, smaller
+    
+bigger:
+lw t0, 0(s0)
+    addi s0, s0, 4
+    j loop_start
+smaller:
+    lw s0, 0(x0)
+    addi s0, s0, 4
+    j loop_start
 
 loop_end:
-    
 
     # Epilogue
-
-
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    addi sp, sp, 12
     ret
--- relu.s
+++ relu.s
@@ -4,32 +4,41 @@
 # ==============================================================================
 # FUNCTION: Performs an inplace element-wise ReLU on an array of ints
 # Arguments:
-# 	a0 is the pointer to the array
-#	a1 is the # of elements in the array
+#   a0 is the pointer to the array
+#   a1 is the # of elements in the array
 # Returns:
-#	None
+#   None
 # ==============================================================================
 relu:
     # Prologue
-
+    addi sp, sp, -12
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    add s0, x0, a0 #address of pointer
+    lw s1, 0(a1) #size of pointer
+    add s2, x0, x0 #counter of pointer
 
 loop_start:
-    
-
-
-
-
-
-
+    addi s2, s2, 1
+    bge s2, s1, loop_end
+    j loop_continue
 
 loop_continue:
-
-
-
-loop_end:
-
-
-    # Epilogue
-
+    lw t0, 0(s0)
+    bge t0, x0, bigger
+    blt t0, x0, smaller
     
-	ret
+bigger:
+    addi s0, s0, 4
+    j loop_start
+smaller:
+    lw s0, 0(x0)
+    addi s0, s0, 4
+    j loop_start
+loop_end:
+    # Epilogue
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    addi sp, sp, 12
