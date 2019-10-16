9efa615dc64af5776ff338db65028e3c7af3720e
Eric Zhang <ezhang3742@berkeley.edu>
2019-10-15 19:11:09 -0700

    fufufu

--- test_files/test_matmul.s
+++ test_files/test_matmul.s
@@ -11,18 +11,31 @@ d: .word 0 0 0 0 0 0 0 0 0 # allocate static space for output
 .text
 main:
     # Load addresses of input matrices (which are in static memory), and set their dimensions
-    
+    la s0 m0
+    addi s1 x0 3 # number of rows m0 (height)
+    addi s2 x0 3 #number of columns m0 (width)
+    la s3 m1
+    addi s4 x0 3 #number of rows m1 (height)
+    addi s5 x0 3 #number of columns m1 (width)
+    la s6 d
 
 
 
     # Call matrix multiply, m0 * m1
-    
-
-
+    mv a0 s0
+    mv a1 s1
+    mv a2 s2
+    mv a3 s3
+    mv a4 s4
+    mv a5 s5
+    mv a6 s6
+    jal ra matmul
 
     # Print the output (use print_int_array in utils.s)
-
-
+    mv a0 s0
+    mv a1 s1
+    mv a2 s2
+    jal print_int_array
 
 
     # Exit the program
