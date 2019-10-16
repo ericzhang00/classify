fd3fd8d8c9161330134a3bd1dfc272acad826dc4
Eric Zhang <ezhang3742@berkeley.edu>
2019-10-16 00:51:50 -0700

    hghggh

--- matmul.s
+++ matmul.s
@@ -19,7 +19,7 @@
 matmul:
 
     # Error if mismatched dimensions
-    bne a1, a5, mismatched_dimensions
+    bne a2, a4, mismatched_dimensions
     # Prologue
     addi sp, sp, -24
     sw s0, 0(sp)
@@ -31,7 +31,7 @@ matmul:
     add s0, a0, x0 # address
     add s1, a1, x0 # height or number of rows of m0
     add s2, a2, x0 # width or number of cols of m0 [STRIDE of v0]
-    add s3, a3, x0 # address of pointer 
+    add s3, a3, x0 # address of pointer
     add s4, a4, x0 # height or number of rows of m1 [STRIDE OF V2]
     add s5, a5, x0 # width or number of cols of m2
     add s6, a6, x0 #pointer to start of d
@@ -64,7 +64,7 @@ outer_loop_end:
     lw s4, 16(sp)
     lw s5, 20(sp)
     addi sp, sp, 24
-    
+
     ret
 
 
