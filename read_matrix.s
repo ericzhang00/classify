2323e2e67c1c74a7d0eb3d6d66802ef9074e93cf
Eric Zhang <ezhang3742@berkeley.edu>
2019-10-20 03:05:21 -0700

    reeee

--- read_matrix.s
+++ read_matrix.s
@@ -19,21 +19,73 @@
 read_matrix:
 
     # Prologue
-	
+    addi sp, sp, -16
+    sw s0, 0(sp)
+	  sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp)
 
+    add s0, x0, a0
+    add s1, x0, a1
+    add s2, x0, a2
 
+    addi a0, x0, 4
+    malloc #a0 now points to 4 bytes
+    add s1, x0, a0
 
+    addi a0, x0, 4
+    malloc #a0 now points to 4 bytes
+    add s2, x0, a0
 
+    add a1, x0, s0  #a1 is pointer to string with filename
+    add a2, x0, x0  #a2 is integer denoting read only
 
+    fopen
+    addi t0, x0, -1
+    beq a0, t0, eof_or_error  #error if open fails
 
+    #a0 now file descriptor
+
+    add a1, a0, x0
+
+    add a2, s1, x0
+    addi a3, x0, 4
+    fread
+
+    add a2, s2, x0
+    addi a3, x0, 4
+    fread
+
+    lw t1, 0(s1)
+    lw t2, 0(s2)
+    mul t3, t1, t2
+    slli t3, t3, 2
+    add a0, t3, x0
+    malloc  #allocates rows x columns x 4 bytes for matrix
+    #a0 now pointer to allocated bytes
+    add s3, x0, a0
+
+    add a2, s3, x0
+    add a3, t3, x0
+    fread
+    bne a0, a3, eof_or_error
+
+    fclose
+    addi t4, x0, -1
+    beq t4, a0, eof_or_error
+
+    add a0, s3, x0
 
 
     # Epilogue
-
+    lw s0, 0(sp)
+	  lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    addi sp, sp, 16
 
     ret
 
 eof_or_error:
     li a1 1
     jal exit2
-    
--- write_matrix.s
+++ write_matrix.s
@@ -20,21 +20,57 @@
 write_matrix:
 
     # Prologue
+    addi sp, sp, -20
+    sw s0, 0(sp)
+    sw s1, 0(sp)
+    sw s2, 0(sp)
+    sw s3, 0(sp)
+    sw s4, 0(sp)
+    add s0, a0, x0
+    add s1, a1, x0
+    sw a2, 0(s2)
+    sw a3, 0(s3)
+    mul s4, a2, a3 #number of elements in matrix
 
+    add a1, s0, x0
+    addi a2, x0, 1
+    fopen
+    addi t0, x0, -1
+    beq a0, t0, eof_or_error  #error if open fails
 
+    add a1, a0, x0
+    add a2, x0, s2
+    addi a3, x0, 1
+    addi a4, x0, 4
+    fwrite
+    bne a0, a3, eof_or_error
+    add a2, x0, s3
+    addi a3, x0, 1
+    addi a4, x0, 4
+    fwrite
+    bne a0, a3, eof_or_error
 
+    add a2, x0, s1
+    add a3, x0, s4
+    addi a4, x0, 4
+    fwrite
+    bne a0, a3, eof_or_error
 
-
-
-
+    fclose
+    addi t1, x0, -1
+    beq t1, a0, eof_or_error
 
 
     # Epilogue
-
+    lw s0, 0(sp)
+    lw s1, 0(sp)
+    lw s2, 0(sp)
+    lw s3, 0(sp)
+    lw s4, 0(sp)
+    addi sp, sp, 20
 
     ret
 
 eof_or_error:
     li a1 1
     jal exit2
-    
