9f648b59a1c69b843f5e442f1211223d753f3b87
Eric Zhang <ezhang3742@berkeley.edu>
2019-10-23 10:51:45 -0700

    sksksksks

--- test_files/test_read_matrix.s
+++ test_files/test_read_matrix.s
@@ -6,6 +6,12 @@ file_path: .asciiz "./test_input.bin"
 
 .text
 main:
+
+    addi sp, sp, -12
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+
     # Read matrix into memory
     #Fairly Confident it works
     #row memory
@@ -22,12 +28,26 @@ main:
     add a1, t0, x0
     add a2, t1, x0
 
+    add s0, a0, x0
+    add s1, a1, x0
+    add s2, a2, x0
+
     # Print out elements of matrix
     jal read_matrix #gets matrix values and a0 pointer is set to that
+
+    add a0, s0, x0
+    add a1, s1, x0
+    add a2, s2, x0
+
     jal print_int_array#prints individual integer valuez
 
     # Terminate the program
     addi a0, x0, 10
     ecall
+
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    addi sp, sp, 12
 
     jal exit
